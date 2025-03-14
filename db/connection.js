1fe8ea57be372981663c680e0216e3d223c9c897
heliosasdesigner <heliogramlaboratory@gmail.com>
2025-03-14 11:54:16 +0000

    connection and package.json update

--- db/connection.js
+++ db/connection.js
@@ -3,8 +3,8 @@ const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
 
 require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
-
-const db = new Pool();
+const config = {};
+const db = new Pool(config);
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("No PGDATABASE or or DATABASE_URL configured");
@@ -16,5 +16,7 @@ if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
+
+db.config = config;
 
 module.exports = db;
